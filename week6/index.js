5cee382a6d3d2444a811c037d197efbfe2fe9be0
macsalinas31 <118233454+macsalinas31@users.noreply.github.com>
2022-12-12 11:41:36 +0800

    changes

--- week6/index.js
+++ week6/index.js
@@ -3,21 +3,19 @@ let num1 = prompt("Enter the first number:");
     num1 = +num1; //this convert the string type to number
 let num2 = prompt("Enter the second number:");
     num2 = +num2;
-let num3 = prompt("Enter the third number:");
-    num3 = +num3;
 // prompt the user to enter the operator
 let operator = prompt("Enter the operator (+, -, *, /):");
 
 // compute the result based on the operator
 let result;
 if (operator === "+") {
-    result = num1 + num2 + num3;
+    result = num1 + num2;
 } else if (operator === "-") {
-    result = num1 - num2 - num3;
+    result = num1 - num2;
 } else if (operator === "*") {
-    result = num1 * num2 * num3;
+    result = num1 * num2;
 } else if (operator === "/") {
-    result = num1 / num2 / num3;
+    result = num1 / num2;
 }
 
 	// display the result using alert
