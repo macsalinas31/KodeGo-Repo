e0a4b35702facc88433b4c4e28dc49c3f26f1767
macsalinas31 <118233454+macsalinas31@users.noreply.github.com>
2022-12-12 11:36:56 +0800

    changes made

---
+++ week5/12092022/js.js
@@ -0,0 +1,5 @@
+const firstName = "Mac";
+     let = `Hello, my name is ${ firstName }`;
+     console.log(firstName)
+     alert("Hello Vlog!");
+     prompt("This is my guys")
--- week6/index.js
+++ week6/index.js
@@ -2,21 +2,22 @@
 let num1 = prompt("Enter the first number:");
     num1 = +num1; //this convert the string type to number
 let num2 = prompt("Enter the second number:");
-    num2 = +num2;//this convert the string type to number
-
+    num2 = +num2;
+let num3 = prompt("Enter the third number:");
+    num3 = +num3;
 // prompt the user to enter the operator
 let operator = prompt("Enter the operator (+, -, *, /):");
 
 // compute the result based on the operator
-var result;
+let result;
 if (operator === "+") {
-    result = num1 + num2;
+    result = num1 + num2 + num3;
 } else if (operator === "-") {
-    result = num1 - num2;
+    result = num1 - num2 - num3;
 } else if (operator === "*") {
-    result = num1 * num2;
+    result = num1 * num2 * num3;
 } else if (operator === "/") {
-    result = num1 / num2;
+    result = num1 / num2 / num3;
 }
 
 	// display the result using alert
