d8e8a1e0f9f0674a15efb073d2f87436398b3753
macsalinas31 <118233454+macsalinas31@users.noreply.github.com>
2023-01-11 10:56:53 +0800

    changes made in activity

--- 01102023/activitycalculator/calculator.js
+++ 01102023/activitycalculator/calculator.js
@@ -1,28 +1,43 @@
-let display = document.getElementById('display');
+//selector
+const output = document.querySelector('.output');
+const result = document.querySelector('.result');
+const keys = document.querySelectorAll('button');
 
-let buttons = Array.from(document.getElementsByClassName('button'));
-
-buttons.map( button => {
-    button.addEventListener(
-        'click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error!"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
+//eventlistener
+keys.forEach(
+    (key) =>{
+    key.addEventListener("click",calculate);
 });
+
+function calculate(){
+    let buttonText = this.innerText;
+    if(buttonText==="C"){
+        output.innerText = "";
+        result.innerText = "0";
+        result.style.animation = "";
+        output.style.animation = "";
+        return;
+        //can do console.log to check if there's error
+        
+    }
+
+    if(buttonText === "DEL"){
+        output.textContent = output.textContent.substr(0,output.textContent.length-1);
+        return;
+    }
+
+    if(buttonText === "="){
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        output.style.animation = "small 0.5s ease-in-out";
+        result.style.animationFillMode = "forwards";
+        output.style.animationFillMode = "forwards";
+    }
+
+    else{
+        output.textContent += buttonText;
+        return;
+    }
+
+  
+}
