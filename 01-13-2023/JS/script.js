245a127a1f212e30264dbed39fcd682360ff7b6e
macsalinas31 <118233454+macsalinas31@users.noreply.github.com>
2023-01-18 06:01:21 +0800

    changes made

--- 01-13-2023/JS/script.js
+++ 01-13-2023/JS/script.js
@@ -4,7 +4,7 @@ const year = document.querySelector('#year');
 const review = document.querySelector('#review');
 const display = document.querySelector('#display pre');
 const retrieveBtn = document.querySelector('#retrieveBtn');
-const reviewsTable =document.querySelector('#reviewsTable');
+const reviewsTable = document.querySelector('#reviewsTable');
 
 const movieReviews = [];
 
@@ -12,41 +12,43 @@ reviewForm.addEventListener(
     'submit',
     (e) => {
         e.preventDefault();
+        const movieReview = {
+            id: Date.now(),
+            title: title.value,
+            year: year.value,
+            review: review.value
+        }
+        movieReviews.push(movieReview);
+        reviewForm.reset();
+        console.table(movieReviews);
+        const reviewsJSON = JSON.stringify( movieReviews, null, 2 );
+        display.innerText = "\n" + reviewsJSON;
 
-       const movieReview = {
-        title: title.value,
-        year: year.value,
-        review: review.value,
-       }
-
-       movieReviews.push(movieReview)
-       reviewForm.reset();
-
-       console.table(movieReviews);
-
-        // JSON - text or string at may sariling method to convert into JSON string
-       const reviewsJSON = JSON.stringify( movieReviews, null, 2 );
-    //    console.log(reviewsJSON);
-       display.innerText = "\n" + reviewsJSON
-
-    localStorage.setItem("Movie Reviews".reviewsJSON);
+        localStorage.setItem("Movie Reviews", reviewsJSON);
     }
-)
+);
 
 retrieveBtn.addEventListener(
     'click',
     () => {
-        //retreive movie review from local storage
-       const reviews = JSON.parse (localStorage.getItem("Movie Reviews"))
+        // retrieve movie reviews from local storage
+        const reviews =  JSON.parse( localStorage.getItem("Movie Reviews") );
 
-       for (let review of reviews) {
-        //create / insert a table row
+        for (let review of reviews) {
+            // create / insert a new table row
+            const row = reviewsTable.insertRow();
 
-        //create / insert cells inside new table row
+            // create / insert cells inside new table row
+            const idCell = row.insertCell();
+            const titleCell = row.insertCell();
+            const yearCell = row.insertCell();
+            const reviewCell = row.insertCell();
 
-        // insert values to the table cells
-       }
-        
-        console.log(reviews)
+            // insert values to the table cells
+            idCell.innerText = review.id;
+            titleCell.innerText = review.title;
+            yearCell.innerText = review.year;
+            reviewCell.innerText = review.review;
+        }
     }
-)
+);
