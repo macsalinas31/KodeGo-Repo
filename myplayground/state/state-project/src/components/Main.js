24a9d6471352f0fb6c2cff431a93bcd60addca20
macsalinas31 <118233454+macsalinas31@users.noreply.github.com>
2023-01-31 14:33:10 +0800

    chnages made

---
+++ myplayground/state/state-project/src/components/Main.js
@@ -0,0 +1,14 @@
+
+function Main () {
+    return (
+        <div className="main-body">
+            <div className="card">
+                <div className="card-title">States</div>
+                <div className="class-body"></div>
+                <count></count>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
